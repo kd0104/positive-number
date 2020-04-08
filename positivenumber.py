9e9cbc8e09f9373cb09be0166803b722bdc9d7d3
kd0104 <63348860+kd0104@users.noreply.github.com>
2020-04-08 18:40:56 +0530

    positivenumber.py
    to print positive numbers only in python

---
+++ positivenumber.py
@@ -0,0 +1,29 @@
+a=int(input("enter element 1:"))
+
+if a>0:
+    print(a);
+
+b=int(input("enter element 2:"))
+
+
+if b>0:
+    print(b);
+
+c=int(input("enter element 3:"))
+
+
+if c>0:
+    print(c);
+
+d=int(input("enter element 4:"))
+
+if d>0:
+    print(d);
+
+e=int(input("enter element 5:"))
+
+
+if e>0:
+    print(e)
+
+
